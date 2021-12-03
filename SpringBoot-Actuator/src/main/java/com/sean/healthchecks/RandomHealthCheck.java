2ad1506ab9a733dcfc7ac6d231bc7b2122861af4
Sean Maxwell <sa.maxwell@yahoo.com>
2021-12-02 19:55:44 -0600

    Added custom health check and management configs

---
+++ SpringBoot-Actuator/src/main/java/com/sean/healthchecks/RandomHealthCheck.java
@@ -0,0 +1,22 @@
+package com.sean.healthchecks;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RandomHealthCheck implements HealthIndicator {
+
+    @Override
+    public Health health() {
+        int errorCode = check();
+        if (errorCode != 0) {
+            return Health.down().withDetail("Error Code", errorCode).build();
+        }
+        return Health.up().build();
+    }
+
+    private int check() {
+        return (int)(10*Math.random()) % 2 == 0 ? 0 : 1;
+    }
+}
